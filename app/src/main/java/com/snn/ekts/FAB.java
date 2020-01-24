355bb472799ea915d56aa3cb76296a0bddafa14e
SNN <snndmr@users.noreply.github.com>
2020-01-24 19:33:43 +0300

    FAB Animation Added

---
+++ app/src/main/java/com/snn/ekts/FAB.java
@@ -0,0 +1,42 @@
+package com.snn.ekts;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class FAB extends FloatingActionButton.Behavior {
+    public FAB(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+
+        if (child.getVisibility() == View.VISIBLE && dyConsumed > 0) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onHidden(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+        } else if (child.getVisibility() == View.INVISIBLE && dyConsumed < 0) {
+            child.show();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
+
